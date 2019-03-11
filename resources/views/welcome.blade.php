8b25affd2b408618e438f7e48c98ed96f377f3fc
vprtm <vprtm1@gmail.com>
2019-03-11 15:26:59 +0700

    ganti header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,36 +64,63 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <div class="bmd-layout-container bmd-drawer-f-l bmd-drawer-overlay">
+			<header class="bmd-layout-header" style="height:100px;">
+				<div class="navbar navbar-light bg-faded" width="auto" height="60px">
+						<button class="btn btn-secondary bmd-btn-icon navbar-toggler" width="auto" height="100px" type="button" data-toggle="drawer" data-target="#dw-s2">
+							<span class="sr-only">Toggle drawer</span>
+							<img src="icon/ikonmenu.png" width="30px" height="30px" class="d-inline-block" alt="" border="1px" style="border:1px;">
+						</button>
+						
+						<a class="navbar-brand" href="index.php">
+							<img src="picture/logo tengah.png" width="auto" height="60px" class="d-inline-block" alt="">
+						</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+						<a class="navbar-brand" href="index.php" style="align:center">
+							<img src="picture/logo.png" width="auto" height="60px" class="d-inline-block" alt="">
+						</a>
+		
+				</div>
+			</header>
+			
+			<div id="dw-s2" class="bmd-layout-drawer bg-faded">
+				<header>
+					<a class="navbar-brand">Main Menu</a>
+					<div class="dropdown-divider"></div>
+				</header>
+				
+				<ul class="list-group nav-pills">
+					<a class="btn btn-primary list-group-item nav-item" href="index.php">HOME</a>
+					<a class="btn btn-primary list-group-item nav-item" data-toggle="collapse" href="#ProfileCollapse" role="button" aria-expanded="false" aria-controls="ProfileCollapse"> PROFILE </a>
+						<div class="collapse" id="ProfileCollapse">
+							<div class="dropdown-divider"></div>
+								<a class="list-group-item dropdown-item" href="?module=profile#pos">User</a>
+								<a class="list-group-item dropdown-item" href="?module=place#pos">Place</a>
+							<div class="dropdown-divider"></div>
+						</div>
+						<a class="btn btn-primary list-group-item nav-item" data-toggle="collapse" href="#AccountCollapse" role="button" aria-expanded="false" aria-controls="AccountCollapse"> ACCOUNT </a>
+					
+						<div class="collapse" id="AccountCollapse">
+							<div class="dropdown-divider"></div>
+								<a class="list-group-item dropdown-item" href="?module=login#pos">Login</a>
+								<a class="list-group-item dropdown-item" href="?module=register#pos">Register</a>
+								<a class="list-group-item dropdown-item" href="?module=forgotpass#pos">Forgot Password</a>
+							<div class="dropdown-divider"></div>
+						</div>
+				</ul>
+			</div>
+	  
+	  
+		<main class="bmd-layout-content">
+		
+			<?php if(isset($_GET['module']))
+						include "konten/$_GET[module].php";
+					else
+						include "konten/home.php";?>
+		
+			</div>
+		
+		</main>
+	</div>
     </body>
 </html>
